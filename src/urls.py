4c9f2edf38942cf8d9530c8ff730f356a9d145b6
jamersonwalderson <jamersonwalderson@gmail.com>
2023-02-01 18:28:59 -0300

    fix: corrijingo importação não usada

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from src.apps.service.api.v1.views import ServiceViewSet
+from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r"services", ServiceViewSet, basename="Servicos")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/", include(router.urls)),
