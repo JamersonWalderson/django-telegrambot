d943f17f3a1b3a161e9782da59c2b64814cadbb1
jamersonwalderson <jamersonwalderson@gmail.com>
2023-02-01 18:23:12 -0300

    chore: mudando nome do script

--- startbot.py
+++ startbot.py
@@ -4,6 +4,7 @@ CHAVE_API = "6039607999:AAFEmx_ZnSduVfdmT1gdU3SWduUoX1_4m7A"
 
 bot = telebot.TeleBot(CHAVE_API)
 
+
 @bot.message_handler(commands=["cardapio"])
 def cardapio(message):
     text = """O que você quer?\n
@@ -13,11 +14,13 @@ def cardapio(message):
     """
     bot.reply_to(message, text)
 
+
 @bot.message_handler(commands=["pizza"])
 def pizza(message):
     bot.reply_to(message, "Saindo a pizza! O tempo de espera é de 20 minutos.")
     bot.send_message(message.chat.id, "Agradecemos seu pedido ver novamente.")
     bot.send_message(message.chat.id, "Caso queria voltar ao menu digite /menu")
+
 
 @bot.message_handler(commands=["burguer"])
 def burguer(message):
@@ -25,11 +28,13 @@ def burguer(message):
     bot.send_message(message.chat.id, "Agradecemos seu pedido ver novamente.")
     bot.send_message(message.chat.id, "Caso queria voltar ao menu digite /menu")
 
+
 @bot.message_handler(commands=["salada"])
 def salada(message):
     bot.reply_to(message, "Saindo a salada! O tempo de espera é de 20 minutos.")
     bot.send_message(message.chat.id, "Agradecemos seu pedido ver novamente.")
     bot.send_message(message.chat.id, "Caso queria voltar ao menu digite /menu")
+
 
 @bot.message_handler(commands=["reclamar"])
 def reclamar(message):
@@ -37,11 +42,13 @@ def reclamar(message):
     jamersonwalderson@gmail.com"""
     bot.reply_to(message, text)
 
+
 @bot.message_handler(commands=["abraco"])
 def abraco(message):
-    text =f"{message.from_user.first_name}, é um prazer té-lo como nosso cliente e amigo, sinta-se abraçado por toda nossa equipe :)"
+    text = f"{message.from_user.first_name}, é um prazer té-lo como nosso cliente e amigo, sinta-se abraçado por toda nossa equipe :)"
     print(message)
     bot.reply_to(message, text)
+
 
 @bot.message_handler(commands=["dev"])
 def dev(message):
@@ -52,20 +59,19 @@ def dev(message):
     bot.reply_to(message, text)
 
 
-
 def init(message):
     return True
+
 
 @bot.message_handler(func=init)
 def start_message(message):
     welcome = """
-        Óla, para iniciar seu atendimento escolha uma opção:
-        /reclamar Reclamar de um pedido.
-        /abraco Mandar um abraço.
-        /dev Informações sobre o desenvolvedor.
-
-        Respo
+    Óla, para iniciar seu atendimento escolha uma opção:
+    /reclamar Reclamar de um pedido.
+    /abraco Mandar um abraço.
+    /dev Informações sobre o desenvolvedor.
     """
     bot.send_message(chat_id=message.chat.id, text=welcome)
+
 
 bot.polling()
